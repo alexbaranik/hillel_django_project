4e1352430bff06966b95d7393eb036070fa1b804
alexbaranik <baranik76@ukr.net>
2022-10-06 17:22:59 +0300

    fix image_snapshot

--- shop/mixins/admin_mixins.py
+++ shop/mixins/admin_mixins.py
@@ -3,6 +3,9 @@ from django.utils.safestring import mark_safe
 
 class ImageMixins():
     def image_snapshot(self, instance):
-        return mark_safe(
-            f'<img src="{instance.image.url}" width="64" height="64"/>'
-            )
+        if instance.image and instance.image.url:
+            return mark_safe(
+                f'<img src="{instance.image.url}" width="64" height="64"/>'
+                )
+        return mark_safe('Image missing')
+    
