6110dc10efd184dfbdeea3a02421c5d7b6f4e04a
alexbaranik <baranik76@ukr.net>
2022-10-26 10:54:24 +0300

    celery

--- shop/settings.py
+++ shop/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # external apps
     'django_extensions',
+    'django_celery_results',
     # own apps
     'products',
     'orders',
@@ -151,3 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = reverse_lazy('main')
 LOGIN_REDIRECT_URL = reverse_lazy('main')
 LOGIN_URL = reverse_lazy('login')
+
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = 'django_celery_results.backends.database.DatabaseBackend' # noqa
+CELERY_IMPORTS = ('shop.tasks',)
