473ea4ffb9bad0daa454627995ed75101829b9d2
alexbaranik <baranik76@ukr.net>
2022-12-09 18:33:02 +0200

    parser BeautifulSoup

--- orders/form.py
+++ orders/form.py
@@ -7,3 +7,8 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['user', 'discount']
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].widget = forms.HiddenInput()
+        self.fields['user'].initial = user.id
